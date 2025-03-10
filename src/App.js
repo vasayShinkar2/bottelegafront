170477fb8fba52070fee8f4fadfbde2b279d1a6e
vasayShinkar2 <vasay0006@gmail.com>
2025-03-10 17:10:04 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      расписание :
+      9:00 - 10:00 - физика
+      10:20 - 12:20 -  химия
+
     </div>
   );
 }
